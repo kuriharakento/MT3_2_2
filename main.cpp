909d7f001135f095c6ebdfad553e9bbe4988af9a
kuriharakento <146906405+kuriharakento@users.noreply.github.com>
2024-05-23 23:29:54 +0900

    プロジェクト ファイルを追加します。

---
+++ main.cpp
@@ -0,0 +1,118 @@
+#include <imgui.h>
+#include <Novice.h>
+#include <MyFunctions.h>
+
+const char kWindowTitle[] = "LC1A_13_クリハラ_ケント_タイトル";
+
+// Windowsアプリでのエントリーポイント(main関数)
+int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
+
+	// ライブラリの初期化
+	Novice::Initialize(kWindowTitle, 1280, 720);
+
+	// キー入力結果を受け取る箱
+	char keys[256] = {0};
+	char preKeys[256] = {0};
+
+	static const int kWindowWidth = 1280;
+	static const int kWindowHeight = 720;
+
+	Vector3 cameraTranslate{ 0.0f,1.9f,-6.49f };
+	Vector3 cameraRotate{ 0.26f,0.0f,0.0f };
+
+	Plane plane{ {0.0f,1.0f,0.0f},1.0f };
+	Sphere sphere{ Vector3{},0.5f,WHITE };
+
+	float cameraSpeed = 0.01f;
+
+	// ウィンドウの×ボタンが押されるまでループ
+	while (Novice::ProcessMessage() == 0) {
+		// フレームの開始
+		Novice::BeginFrame();
+
+		// キー入力を受け取る
+		memcpy(preKeys, keys, 256);
+		Novice::GetHitKeyStateAll(keys);
+
+		///
+		/// ↓更新処理ここから
+		///
+
+		ImGui::Begin("window");
+		ImGui::DragFloat3("Sphere.Center", &sphere.center.x, 0.01f);
+		ImGui::DragFloat("Sphere.Radius", &sphere.radius, 0.01f);
+		ImGui::DragFloat3("Plane.Normal", &plane.normal.x, 0.01f);
+		ImGui::DragFloat("Plane.Distance", &plane.distance, 0.01f);
+		ImGui::End();
+
+		plane.normal = Normalize(plane.normal);
+
+		if (keys[DIK_W])
+		{
+			cameraTranslate.z += cameraSpeed;
+		}
+		if (keys[DIK_S])
+		{
+			cameraTranslate.z -= cameraSpeed;
+		}
+		if (keys[DIK_A])
+		{
+			cameraTranslate.x -= cameraSpeed;
+		}
+		if (keys[DIK_D])
+		{
+			cameraTranslate.x += cameraSpeed;
+		}
+		if (keys[DIK_Q])
+		{
+			cameraTranslate.y += cameraSpeed;
+		}
+		if (keys[DIK_E])
+		{
+			cameraTranslate.y -= cameraSpeed;
+		}
+
+		Matrix4x4 cameraMatrix = MakeAffineMatrix({ 1.0f,1.0f,1.0f }, cameraRotate, cameraTranslate);
+		Matrix4x4 viewMatriix = Inverse(cameraMatrix);
+		Matrix4x4 projectionMatrix = MakePerspectiveFovMatrix(0.45f, (float)kWindowWidth / (float)kWindowHeight, 0.1f, 100.0f);
+		Matrix4x4 viewProjectionMatrix = Multiply(viewMatriix, projectionMatrix);
+		Matrix4x4 viewportMatrix = MakeViewportMatrix(0, 0, (float)kWindowWidth, (float)kWindowHeight, 0.0f, 1.0f);
+
+		if (IsCollision(sphere, plane))
+		{
+			sphere.color = RED;
+		}
+		else
+		{
+			sphere.color = WHITE;
+		}
+
+		///
+		/// ↑更新処理ここまで
+		///
+
+		///
+		/// ↓描画処理ここから
+		///
+
+		DrawGrid(viewProjectionMatrix, viewportMatrix);
+		DrawSphere(sphere, viewProjectionMatrix, viewportMatrix, sphere.color);
+		DrawPlane(plane, viewProjectionMatrix, viewportMatrix, WHITE);
+
+		///
+		/// ↑描画処理ここまで
+		///
+
+		// フレームの終了
+		Novice::EndFrame();
+
+		// ESCキーが押されたらループを抜ける
+		if (preKeys[DIK_ESCAPE] == 0 && keys[DIK_ESCAPE] != 0) {
+			break;
+		}
+	}
+
+	// ライブラリの終了
+	Novice::Finalize();
+	return 0;
+}
